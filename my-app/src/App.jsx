b614384bf74d1631a1b3969ae38b98b542ff53fa
Abdiel <131422484+AbdielFritsche@users.noreply.github.com>
2025-05-15 12:08:39 -0600

    routes

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -30,7 +30,7 @@ function App() {
   const getItems = async () => {
     try {
       const startTime = Date.now();
-      const result = await fetch("http://localhost:5000/items/", { 
+      const result = await fetch("https://backenddeployreactactivities-production.up.railway.app/items_mongo/", { 
           method: "GET",
           headers: {
             'Authorization': `Bearer ${token}`
@@ -53,7 +53,7 @@ function App() {
   const decrement = () => setCount(count - 1);
 
   const add = async (item) => {
-    const result = await fetch("http://localhost:5000/items/", {
+    const result = await fetch("https://backenddeployreactactivities-production.up.railway.app/items_mongo/", {
       method: "POST",
       headers: { 
         'Authorization': `Bearer ${token}`,
@@ -79,7 +79,7 @@ function App() {
     }
   
     try {
-      const response = await fetch(`http://localhost:5000/items/${id}`, { 
+      const response = await fetch(`https://backenddeployreactactivities-production.up.railway.app/items_mongo/${id}`, { 
         method: "DELETE",
         headers: {
           'Authorization': `Bearer ${token}`
@@ -96,7 +96,7 @@ function App() {
   
 
   const login = async (user) => {
-    const result = await fetch("http://localhost:5000/login/", {
+    const result = await fetch("https://backenddeployreactactivities-production.up.railway.app/mongo/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(user)
@@ -115,7 +115,7 @@ function App() {
   };
 
   const register = async (user) => {
-    const result = await fetch("http://localhost:5000/register/", {
+    const result = await fetch("https://backenddeployreactactivities-production.up.railway.app/mongo/register", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(user),
